1ee995c206c988081aef40fd3bc959f6fd8bb2b7
jaskrrish <jashehz@gmail.com>
2025-12-21 19:42:32 +0530

    ref: cors addition

--- src/configs/cors.ts
+++ src/configs/cors.ts
@@ -1,5 +1,10 @@
 export const corsConfig = {
-  origin: ['https://localhost:3000', 'https://example.com'],
+  origin: [
+    'https://localhost:3000',
+    'https://pro-beauty-web.vercel.app',
+    'https://pro-beauty-salon-dashboard.vercel.app',
+    'https://pro-beauty-admin.vercel.app',
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
   credentials: true,
